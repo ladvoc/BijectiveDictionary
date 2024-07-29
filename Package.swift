a5ed434d678bf08b394f0f72c474d647e8279c84
ladvoc <3182119+ladvoc@users.noreply.github.com>
2024-07-29 14:03:28 -0700

    Create package manifest

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "BijectiveDictionary",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    products: [
+        .library(
+            name: "BijectiveDictionary",
+            targets: ["BijectiveDictionary"])
+    ],
+    targets: [
+        .target(
+            name: "BijectiveDictionary"),
+        .testTarget(
+            name: "BijectiveDictionaryTests",
+            dependencies: ["BijectiveDictionary"]
+        ),
+    ]
+)
