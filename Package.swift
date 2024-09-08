c216c4d7c9d585049c355d3fde8e96d66c15698a
ladvoc <3182119+ladvoc@users.noreply.github.com>
2024-09-08 12:25:48 -0700

    Only use SwiftLint as a package plugin on macOS
    `SwiftLintPlugins` currently only provides a binary for macOS.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,14 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
+var dependencies: [Package.Dependency] = []
+var plugins: [Target.PluginUsage]?
+
+#if os(macOS)
+dependencies = [.package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.56.2")]
+plugins = [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+#endif
+
 let package = Package(
     name: "BijectiveDictionary",
     platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
@@ -9,20 +17,18 @@ let package = Package(
             name: "BijectiveDictionary",
             targets: ["BijectiveDictionary"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.56.2")
-    ],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "BijectiveDictionary",
             swiftSettings: [.enableExperimentalFeature("StrictConcurrency")],
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            plugins: plugins
         ),
         .testTarget(
             name: "BijectiveDictionaryTests",
             dependencies: ["BijectiveDictionary"],
             swiftSettings: [.enableExperimentalFeature("StrictConcurrency")],
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            plugins: plugins
         ),
     ],
     swiftLanguageVersions: [.v5]
--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -1,6 +1,14 @@
 // swift-tools-version: 6.0
 import PackageDescription
 
+var dependencies: [Package.Dependency] = []
+var plugins: [Target.PluginUsage]?
+
+#if os(macOS)
+dependencies = [.package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.56.2")]
+plugins = [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+#endif
+
 let package = Package(
     name: "BijectiveDictionary",
     platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
@@ -9,18 +17,16 @@ let package = Package(
             name: "BijectiveDictionary",
             targets: ["BijectiveDictionary"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.56.2")
-    ],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "BijectiveDictionary",
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            plugins: plugins
         ),
         .testTarget(
             name: "BijectiveDictionaryTests",
             dependencies: ["BijectiveDictionary"],
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            plugins: plugins
         ),
     ],
     swiftLanguageVersions: [.v6]
